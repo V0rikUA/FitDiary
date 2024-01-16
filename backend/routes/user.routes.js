1d057c4cf0de621251fa01a1e6d3cc4a2a6a5f0a
Dmitriy Kirov <kirovdmitriy@gmail.com>
2024-01-16 22:18:30 +0200

    patch: added auth middleware

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,9 @@
 const { getUserData, addNewUser } = require("../controller/user.controller");
+const { authentication } = require("../utils/auth.js");
 
 const userRouter = require("express").Router();
 
-userRouter.get("/user", getUserData);
+userRouter.get("/user", authentication, getUserData);
 userRouter.post("/user", addNewUser);
 // userRouter.patch("/user/:id", updateUserData);
 // userRouter.delete("/user/:id", deleteUserData);
